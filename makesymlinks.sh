fdd4c1d132037527a0aa747cf0f75a0c06f6aebf
dwhweb <dwh@dwhweb.org>
2016-11-05 17:25:52 +0000

    Change to default config directory

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 
 ########## Variables
 
-dir=~/.dotfiles                    		# dotfiles directory
+dir=~/dotfiles                    		# dotfiles directory
 olddir=~/dotfiles_old             		# old dotfiles backup directory
 files=$(ls -A | egrep -e '^\.' | egrep -v git)  # dotfiles in the directory the script was invoked from (usually ~/.dotfiles)
 
