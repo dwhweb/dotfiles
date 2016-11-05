538149ad59319750d9f285cefd54cf87f80a176b
dwhweb <dwh@dwhweb.org>
2016-11-05 17:17:07 +0000

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,51 @@
+"Map :W to 'sudo save file'
+command W w !sudo tee % >/dev/null
+
+set nocompatible              " be iMproved, required
+filetype off                  " required
+set t_Co=256
+syntax on
+set background=dark
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'Distinguished'
+Plugin 'vim-airline/vim-airline'
+Plugin 'tpope/vim-fugitive'
+Plugin 'scrooloose/nerdtree'
+Plugin 'vim-airline/vim-airline-themes'
+Plugin 'Raimondi/delimitMate'
+Plugin 'myusuf3/numbers.vim'
+
+call vundle#end()            " required
+filetype plugin indent on    " required
+
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
+:silent! colorscheme distinguished "Set the colour scheme
+set laststatus=2 "Enables vim-airline all the time
+set number "Enables line numbers all the time
+let g:airline_powerline_fonts = 1 "Populate symbol dictionary with powerline symbols for vim-airline
+let g:airline#extensions#tabline#enabled = 1 "Turn on buffer line at top of window
+let g:airline#extensions#tabline#buffer_nr_show = 1 "Show buffer numbers in buffer line
+let g:airline_theme='distinguished' "Set airline theme
+let delimitMate_expand_cr = 2 "Setup delimitmate to automatically expand delimiters on <CR> 
+
+"Map F3 to turn relative numbering on and off
+nnoremap <F3> :NumbersToggle<CR>
---
+++ makesymlinks.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+############################
+# .makesymlinks.sh
+# Backs up existing dotfiles (if required) then creates symlinks to all the dotfiles contained within the
+# directory this script was invoked from (usually ~/.dotfiles)
+############################
+
+########## Variables
+
+dir=~/.dotfiles                    		# dotfiles directory
+olddir=~/dotfiles_old             		# old dotfiles backup directory
+files=$(ls -A | egrep -e '^\.' | egrep -v git)  # dotfiles in the directory the script was invoked from (usually ~/.dotfiles)
+
+##########
+
+# Check if user wants to create backups
+while true; do
+	read -p "Do you want to backup any existing dotfiles to $olddir? (Yes/No)" yn
+    
+	mkdir $olddir 2> /dev/null
+
+	case $yn in
+		[Yy]* ) for file in $files; do
+				if [ -e ~/$file ]; then
+					echo -e "Copying  ~/$file to $olddir"
+					cp -L -R ~/$file $olddir/
+				fi
+			done;
+			break;;
+		[Nn]* ) break;;
+		* ) echo -e "Please answer yes or no.";;
+    	esac
+done
+
+# Create symlinks
+for file in $files; do
+    echo -e "Creating symlink to $file in home directory."
+    ln -s -f $dir/$file ~/$file
+done
+
+# Prompt to install Vundle and plugins, if ,vimrc exists
+if [ -e ~/.vimrc ]; then
+	while true; do
+		read -p "Do you want to install Vundle and plugins? (Yes/No)" yn
+	    case $yn in
+		[Yy]* ) 
+			if [ -d ~/.vim/bundle/Vundle.vim ]; then
+				while true; do
+					read -p "Do you want to backup your existing Vundle dir? (Yes/No)" yn
+				    case $yn in
+					[Yy]* ) cp -fR ~/.vim/bundle/Vundle.vim $olddir; break;;
+					[Nn]* ) break;;
+					* ) echo -e "Please answer yes or no.";;
+				    esac
+				done
+			fi
+
+			rm -fR ~/.vim/bundle/Vundle.vim	
+			git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim; vim +PluginInstall +qall; break;;
+		[Nn]* ) break;;
+		* ) echo -e "Please answer yes or no.";;
+	    esac
+	done
+fi
+
+# Prompt to restart bash, if config exists
+if [ -e ~/.bashrc ]; then
+	while true; do
+		read -p "Do you want to restart bash? (Yes/No)" yn
+	    case $yn in
+		[Yy]* ) exec bash; break;;
+		[Nn]* ) exit;;
+		* ) echo -e "Please answer yes or no.";;
+	    esac
+	done
+fi
